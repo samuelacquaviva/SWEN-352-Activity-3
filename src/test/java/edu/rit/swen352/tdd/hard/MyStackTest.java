fc683f3b73643406c435f7cda87f5dc924d9bb08
Alan Li <al9269@rit.edu>
2025-09-30 15:53:03 -0400

    MyStack : 1 : construct with default capacity : create failing test

--- src/test/java/edu/rit/swen352/tdd/hard/MyStackTest.java
+++ src/test/java/edu/rit/swen352/tdd/hard/MyStackTest.java
@@ -1,13 +1,20 @@
 package edu.rit.swen352.tdd.hard;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test suite for the {@link MyStack} component.
  */
 class MyStackTest {
+  @Test
+  void constructStackWithDefaultCapacity() {
+    MyStack<Integer> stack = new MyStack<>();
+    assertEquals(16, stack.getCapacity());
+    assertTrue(stack.isEmpty());
+    assertEquals(0, stack.size());
+  }
 
 }
