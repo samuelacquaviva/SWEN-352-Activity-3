3b699686891527cec68679d0d65aa5e0b3862157
James Ji <57771801+Sannnji@users.noreply.github.com>
2025-10-04 17:27:19 -0400

    SimpleBankAccount : 1a : constructed has 0 balance : create failing test

--- src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
@@ -1,13 +1,26 @@
 package edu.rit.swen352.tdd.easy;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test suite for the {@link SimpleBankAccount} component.
  */
 class SimpleBankAccountTest {
+  private SimpleBankAccount sba = new SimpleBankAccount();
 
+  @BeforeEach
+  void setup() {
+    sba = new SimpleBankAccount();;
+  }
+
+  @Test
+  @DisplayName("Constructor should set balance to 0 ")
+  void testConstructor() {
+    float expected = 0;
+    assertEquals(expected, sba.getBalance());
+  }
 }
