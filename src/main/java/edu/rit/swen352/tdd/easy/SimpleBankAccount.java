034bee5c8415b5ed0f97822dc386fd6758cbfcf9
James Ji <57771801+Sannnji@users.noreply.github.com>
2025-10-04 18:33:41 -0400

    SimpleBankAccount : 4b : withdraw()  : create passing code

--- src/main/java/edu/rit/swen352/tdd/easy/SimpleBankAccount.java
+++ src/main/java/edu/rit/swen352/tdd/easy/SimpleBankAccount.java
@@ -1,24 +1,5 @@
 package edu.rit.swen352.tdd.easy;
 
-/**
- * An American bank account that permits deposits and withdrawals.
- * The balance must never be negative.
- *
- * <p>
- * You must implement these features:
- * <ul>
- *   <li>constructor:
- *     <ul>
- *       <li>with both an initial balance as {@code float} value</li>
- *       <li>a no-arg ctor that sets the value to zero</li>
- *     </ul>
- *   </li>
- *   <li>getBalance: returns the current balance</li>
- *   <li>isAccountEmpty(): queries whether the balance is zero</li>
- *   <li>deposit(amount): add a {@code float} amount to the balance</li>
- *   <li>withdraw(amount): subtract a {@code float} amount from the balance</li>
- * </ul>
- */
 public class SimpleBankAccount {
   private float balance;
 
@@ -36,5 +17,10 @@ public class SimpleBankAccount {
 
   void deposit(float amount) {
     balance += amount;
+  }
+
+  void withdraw(float amount) {
+    float result = Math.round((balance - amount) * 10f) / 10f;
+    balance = result;
   }
 }
