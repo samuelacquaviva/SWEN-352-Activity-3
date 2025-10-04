aebc594ae47c7ffb2b64adb8f7820deefd3869ba
James Ji <57771801+Sannnji@users.noreply.github.com>
2025-10-04 18:35:16 -0400

    SimpleBankAccount : 4b : withdraw()  : removed unused import

--- src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 /**
  * Test suite for the {@link SimpleBankAccount} component.
