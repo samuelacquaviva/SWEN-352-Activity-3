3bafb35e0f23b79c73fc4c44ab4ba8f3f2774efd
James Ji <57771801+Sannnji@users.noreply.github.com>
2025-10-04 21:39:19 -0400

    BankAccount : 4b : withdraw() : create passing code

--- src/main/java/edu/rit/swen352/tdd/hard/BankAccount.java
+++ src/main/java/edu/rit/swen352/tdd/hard/BankAccount.java
@@ -6,25 +6,6 @@ package edu.rit.swen352.tdd.hard;
  */
 record Money(int dollars, int cents) {}
 
-/**
- * An American bank account that permits deposits and withdrawals.
- * The balance must never be negative.
- *
- * <p>
- * You must implement these features:
- * <ul>
- *   <li>constructor:
- *     <ul>
- *       <li>with both an initial balance as {@link Money} value</li>
- *       <li>a no-arg ctor that sets the value to zero</li>
- *     </ul>
- *   </li>
- *   <li>getBalance: returns the current balance</li>
- *   <li>isAccountEmpty(): queries whether the balance is zero</li>
- *   <li>deposit(amount): add a {@link Money} amount to the balance</li>
- *   <li>withdraw(amount): subtract a {@link Money} amount from the balance</li>
- * </ul>
- */
 public class BankAccount {
   Money balance;
 
@@ -32,27 +13,27 @@ public class BankAccount {
     balance = new Money(0, 0);
   }
 
-  int getDollars() {
+  public int getDollars() {
     return balance.dollars();
   }
 
-  int getCents() {
+  public int getCents() {
     return balance.cents();
   }
 
-  String getBalance() {
+  public String getBalance() {
     if (getDollars() == 0 && getCents() == 0)
       return "$0.00";
     return "$" + getDollars() + "." + getCents();
   }
 
-  boolean isAccountEmpty() {
+  public boolean isAccountEmpty() {
     if (getDollars() == 0 && getCents() == 0) 
       return true;
     return false;
   }
   
-  void deposit(Money amount) {
+  public void deposit(Money amount) {
     int newDollarAmount = getDollars() + amount.dollars();
     int newCentAmount = getCents() + amount.cents();
 
@@ -64,4 +45,17 @@ public class BankAccount {
     balance = new Money(newDollarAmount, newCentAmount);
   }
 
+   public void withdraw(Money amount) {
+    int newDollarAmount = getDollars() - amount.dollars();
+    int newCentAmount = getCents() - amount.cents();
+
+    if (newCentAmount < 0) {
+      newDollarAmount -= 1;
+      newCentAmount += 100;
+    } else if (newDollarAmount < 0) {
+      newDollarAmount = 0;
+    }
+    
+    balance = new Money(newDollarAmount, newCentAmount);
+  }
 }
