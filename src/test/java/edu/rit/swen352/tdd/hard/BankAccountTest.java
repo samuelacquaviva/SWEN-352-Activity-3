145def146841a7c7f50d8d6597628ea76da0c1c2
James Ji <57771801+Sannnji@users.noreply.github.com>
2025-10-04 21:00:34 -0400

    BankAccount : 1a : constructed has 0 balance : create failing test

--- src/test/java/edu/rit/swen352/tdd/hard/BankAccountTest.java
+++ src/test/java/edu/rit/swen352/tdd/hard/BankAccountTest.java
@@ -5,9 +5,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 /**
  * Test suite for the {@link BankAccount} component.
  */
 class BankAccountTest {
+  private BankAccount ba;
 
+  @BeforeEach
+  void setup() {
+    ba = new BankAccount();
+  }
+
+  @Test
+  @DisplayName("Constructor should set balance to $0.00 when returned")
+  void testConstructor() {
+    String expected = "$0.00";
+    assertEquals(expected, ba.getBalance());
+  }
 }
