d7a879c5294bcc2292764d22db912fea769901c5
James Ji <57771801+Sannnji@users.noreply.github.com>
2025-10-05 19:08:46 -0400

    remove changes from task 1

--- src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
@@ -1,50 +1,13 @@
 package edu.rit.swen352.tdd.easy;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test suite for the {@link SimpleBankAccount} component.
  */
 class SimpleBankAccountTest {
-  private SimpleBankAccount sba = new SimpleBankAccount();
 
-  @BeforeEach
-  void setup() {
-    sba = new SimpleBankAccount();;
-  }
-
-  @Test
-  @DisplayName("Constructor should set balance to 0 ")
-  void testConstructor() {
-    float expected = 0;
-    assertEquals(expected, sba.getBalance());
-  }
-
-  @Test
-  @DisplayName("Get if account balance is 0")
-  void testIsAccountEmpty() {
-    boolean expected = true;
-    assertEquals(expected, sba.isAccountEmpty());
-  }
-
-  @Test
-  @DisplayName("Deposit updates balance")
-  void testDeposit() {
-    float expected = 5.20f;
-    sba.deposit(5.20f);
-    assertEquals(expected, sba.getBalance());
-  }
-
-  @Test
-  @DisplayName("Withdraw updates balance")
-  void testWithdraw() {
-    sba.deposit(5.20f);
-    sba.withdraw(2.3f);
-    float expected = 2.9f;
-    assertEquals(expected, sba.getBalance());
-  }
 }
